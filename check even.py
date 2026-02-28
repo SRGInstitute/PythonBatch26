6275a675dea6168602370a24bf1e276bbde3af8e
as9891942570-dotcom <as9891942570@gmail.com>
2026-02-28 11:57:35 +0530

    even programme

---
+++ check even.py
@@ -0,0 +1,14 @@
+# number = [4,45,48,5,5,5,6,7,6,8,2]
+
+# smallest =number[0]
+
+# for num in number:
+#     if num < smallest:
+#         smallest=num
+
+# print("smallnumber",smallest)
+
+one = int(input("enter tha value"))
+two=int(input("enter tha value"))
+three=int(input("enter tha value"))
+
