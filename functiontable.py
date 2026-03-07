6c9f038838ca37c44e39889fd60faa8031963d73
as9891942570-dotcom <as9891942570@gmail.com>
2026-03-07 11:10:19 +0530

    table with function

---
+++ functiontable.py
@@ -0,0 +1,60 @@
+
+ 
+
+def table (n):
+    for i in range(1,11):
+        print(n,"x", i, "=", n*i)
+while True:
+    num=int(input("enter tha number"))
+
+    table(num)
+
+
+
+
+
+
+
+
+
+# def table1(num):
+    # return num*1
+    
+
+# def table2(num):
+#     return num*2
+# def table3(num):
+#     return num*3
+# def table4(num):
+#     return num*4
+# def table5(num):
+#     return num*5
+# def table6(num):
+#     return num*6
+# def table7(num):
+#     return num*7
+# def table8(num):
+#     return num*8
+# def table9(num):
+#     return num*9
+# def table10(num):
+#     return num*10
+
+
+# print(table1(num))
+# print(table2(num))
+# print(table3(num))
+# print(table4(num))
+# print(table5(num))
+# print(table6(num))
+# print(table7(num))
+# print(table8(num))
+#print(table9(num))
+# print(table10(num))
+
+
+
+
+
+
+
