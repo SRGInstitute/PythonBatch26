442e600152a0cdfc48e7724611ff9df894d798b1
rachitshukla0001 <rachitshukla0623@gmail.com>
2026-03-08 11:51:59 +0530

    vowles program

---
+++ vowels.py
@@ -0,0 +1,10 @@
+def vowels(text):
+    vowels1=[]
+    foe ch in text:
+        if ch in "aieouAIEOU":
+            vowles1.append9ch)
+    return vowles1
+
+
+text=input("enter string")
+print(vowles(text))
