ebcaad0cc88112b79574391948494c857264a59c
rachitshukla0001 <rachitshukla0623@gmail.com>
2026-03-18 07:55:38 +0530

    3 program done

---
+++ pandas program/indexing.py
@@ -0,0 +1,4 @@
+import pandas as pd
+series1=pd.Series(['a','b','c','d'],index=['c','d','b','a'])
+
+print(series1['c'])
---
+++ pandas program/sum.py
@@ -0,0 +1,8 @@
+import pandas as pd
+series1=pd.Series([20,40,50,60])
+
+print("Sum: ",series1.sum())
+print("Mean: ",series1.mean())
+print("Max: ",series1.max())
+print("Min: ",series1.min())
+print("Count: ",series1.count())
